c23a5dc0e48da7307a8927bebd41baa51664025b
Victor Alexiev <vic.alexiev@gmail.com>
2018-08-07 19:13:20 +0300

    sample project added

---
+++ src/BiscuitMachine/BiscuitMachine.Web/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-component',
+  templateUrl: './dashboard.component.html'
+})
+export class DashboardComponent {
+  public switchOn() { }
+
+  public switchOff() { }
+
+  public pause() { }
+}
